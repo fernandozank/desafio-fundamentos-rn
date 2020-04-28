2d8113e7b6ca1de70fc9514d98067fdb7de6c665
Fernando Zank <zank@pazinatoti.com.br>
2020-04-28 16:25:39 -0300

    Style update

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
@@ -24,7 +24,22 @@ const AppRoutes: React.FC = () => (
       options={{
         headerShown: true,
         headerTransparent: true,
-        headerTitle: () => <Image source={Logo} />,
+        // headerTitle: () => <Image source={Logo} />,
+        headerTitle: () => (
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <FeatherIcon name="shopping-bag" size={35} color="#003399" />
+            <Text style={{ fontSize: 18, marginLeft: 10, color: '#003399' }}>
+              Products List
+            </Text>
+          </View>
+        ),
+        headerTintColor: '#333',
       }}
       name="Dashboard"
       component={Dashboard}
@@ -32,7 +47,21 @@ const AppRoutes: React.FC = () => (
     <App.Screen
       options={{
         headerTransparent: true,
-        headerTitle: () => <Image source={Logo} />,
+        // headerTitle: () => <Image source={Logo} />,
+        headerTitle: () => (
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <FeatherIcon name="shopping-cart" size={35} color="#003399" />
+            <Text style={{ fontSize: 18, marginLeft: 10, color: '#003399' }}>
+              Your Cart
+            </Text>
+          </View>
+        ),
         headerBackTitleVisible: false,
         headerLeftContainerStyle: {
           marginLeft: 20,
